cd961da8c256fbc54e9c6916b728e331e0a02b40
Zaki Ahmad <zaki.ahmad@innoviti.blr>
2021-04-11 20:07:43 +0530

    first commit

---
+++ src/test/MyGitRepo.java
@@ -0,0 +1,10 @@
+package test;
+
+public class MyGitRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello Zaki,Welcome to Git World");
+	}
+
+}
