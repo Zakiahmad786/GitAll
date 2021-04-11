8e2627b2060866ba51c471440873f6a2054dfa63
Zakiahmad786 <81512638+Zakiahmad786@users.noreply.github.com>
2021-04-11 21:05:39 +0530

    Update MyGitRepo.java
    Removed the description

--- src/test/MyGitRepo.java
+++ src/test/MyGitRepo.java
@@ -3,7 +3,6 @@ package test;
 public class MyGitRepo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
        System.out.println("Hello Zaki,Welcome to Git World");
 	}
 
