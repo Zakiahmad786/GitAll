cba95cb64fd13598f30cad3719d3be66425e3fc5
Zaki Ahmad <zaki.ahmad@innoviti.blr>
2021-04-12 08:22:29 +0530

    Addition of one method

--- src/test/MyGitRepo.java
+++ src/test/MyGitRepo.java
@@ -5,5 +5,9 @@ public class MyGitRepo {
 	public static void main(String[] args) {
        System.out.println("Hello Zaki,Welcome to Git World");
 	}
-
+	
+  void test() {
+	  System.out.println("This is change in JavaProgramBranch");
+  }
+	
 }
